7d73f84f7820bdc8c11f2e00e57dfdf05d8e989e
superturboryan <forsyth.r@gmail.com>
2020-06-26 20:49:26 -0400

    Initial Commit

---
+++ Genre-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Genre-SwiftUI
+//
+//  Created by Ryan David Forsyth on 2020-06-26.
+//  Copyright © 2020 Ryan David Forsyth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
